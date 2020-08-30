2bd9219ad0016532edadb53ab73f23a9b0059821
6110110144 <55184541+6110110144@users.noreply.github.com>
2020-08-30 20:48:47 +0700

    UseEffect

--- components/Weather.js
+++ components/Weather.js
@@ -3,6 +3,23 @@ import Forecast from './Forecast'
 import { ImageBackground, StyleSheet, Text } from 'react-native'
 
 export default function Weather(props){
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=268db58a747837b9d617304a0de17a49`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp});
+                })
+            .catch((error) => {
+                console.warn(error);
+            });
+        }
+    } 
+}, [props.zipCode])
     const [forecastInfo, setForecastInfo] = useState({
         main: '-',
         description: '-',
